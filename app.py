f99378afbf72dc6ad859b0dd323790b42190abb6
smithc6136 <smithc6136@yahoo.com>
2020-10-28 16:25:18 -0400

    created functions in scrape_mars

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
 # import necessary libraries
 from flask import Flask, render_template
 import scrape_mars
+# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
+import pymongo
 
 # create instance of Flask app
 app = Flask(__name__)
+
+# Code from 12.3 Activity 7 for Mongo part(??)
+# # Create connection variable
+# conn = 'mongodb://localhost:27017'
+
+# # Pass connection to the pymongo instance.
+# client = pymongo.MongoClient(conn)
+
+# # Connect to a database. Will create one if not already available.
+# db = client.team_db
+
+# # Drops collection if available to remove duplicates
+# db.team.drop()
+
+# # Creates a collection in the database and inserts two documents
+# db.team.insert_many(
+#     [
+#         {
+#             'player': 'Jessica',
+#             'position': 'Point Guard'
+#         },
+#         {
+#             'player': 'Mark',
+#             'position': 'Center'
+#         }
+#     ]
+# )
+
 
 
 # create route that renders index.html template
--- scrape_mars.py
+++ scrape_mars.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 import requests
 from splinter import Browser
+import
 
 executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
 browser = Browser("chrome", **executable_path, headless=False)
@@ -38,6 +39,85 @@ def scrape_image():
     result_link = soup.find(class_= 'main_image').get('src')
     featured_image_url = "https://www.jpl.nasa.org" + result_link
     return {'featured_image': featured_image_url}
+
+def scrape_facts():
+    #Mars facts: URL of page to be scraped
+    facts_url="https://space-facts.com/mars/"
+    tables = pd.read_html(facts_url)
+    # We can slice off any of those dataframes that we want using normal indexing.
+    df = tables[0]
+    df.columns = ['variable', 'measurement']
+    df.head()
+    #Set the index to the State column
+    df.set_index('variable', inplace=True)
+    df.head()
+    # Pandas to_html method generates HTML tables from DataFrames.
+    html_table = df.to_html()
+
+def scrape_hemispheres():
+    #Mars Hemispheres: URL of page to be scraped
+    hemisphere_url='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    # Retrieve page
+    browser.visit(hemisphere_url)
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup = BeautifulSoup(browser.html, 'html.parser')
+    #CERBERUS HEMISPHERE: use splinter to click through website to get to desired image
+    cerberus_hemisphere = browser.links.find_by_partial_text("Cerberus Hemisphere Enhanced")
+    cerberus_hemisphere.click()
+    #use splinter to click through website to get to desired image
+    cerberus_hemisphere_image = browser.links.find_by_partial_text("Sample")
+    cerberus_hemisphere_image.click()
+    #Recreate soup
+    soup = BeautifulSoup(browser.html, 'html.parser')
+    cerberus_link = soup.find('img').get('src')
+    cerberus_full_link = "https://www.jpl.nasa.gov" + cerberus_link
+    # Retrieve page
+    browser.visit(hemisphere_url)
+
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup = BeautifulSoup(browser.html, 'html.parser')
+    #SCHIAPARELLI HEMISPHERE: use splinter to click through website to get to desired image
+    Schiaparelli_hemisphere = browser.links.find_by_partial_text("Schiaparelli Hemisphere Enhanced")
+    Schiaparelli_hemisphere.click()
+    #use splinter to click through website to get to desired image
+    Schiaparelli_hemisphere_image = browser.links.find_by_partial_text("Sample")
+    Schiaparelli_hemisphere_image.click()
+    #Recreate soup
+    soup = BeautifulSoup(browser.html, 'html.parser')
+    Schiaparelli_link = soup.find('img').get('src')
+    Schiaparelli_full_link = "https://www.jpl.nasa.gov" + Schiaparelli_link
+    # Retrieve page
+    browser.visit(hemisphere_url)
+
+
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup = BeautifulSoup(browser.html, 'html.parser')
+    #SYRTIS MAJOR HEMISPHERE: use splinter to click through website to get to desired image
+    Syrtis_Major_hemisphere = browser.links.find_by_partial_text("Syrtis Major Hemisphere Enhanced")
+    Syrtis_Major_hemisphere.click()
+    #use splinter to click through website to get to desired image
+    Syrtis_Major_hemisphere_image = browser.links.find_by_partial_text("Sample")
+    Syrtis_Major_hemisphere_image.click()
+    #Recreate soup
+    soup = BeautifulSoup(browser.html, 'html.parser')
+    Syrtis_Major_link = soup.find('img').get('src')
+    Syrtis_Major_full_link = "https://www.jpl.nasa.gov" + Syrtis_Major_link
+    # Retrieve page
+    browser.visit(hemisphere_url)
+
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup = BeautifulSoup(browser.html, 'html.parser')
+    #VALLES MARINERIS HEMISPHERE: use splinter to click through website to get to desired image
+    Valles_Marineris_hemisphere = browser.links.find_by_partial_text("Valles Marineris Hemisphere Enhanced")
+    Valles_Marineris_hemisphere.click()
+    #use splinter to click through website to get to desired image
+    Valles_Marineris_hemisphere_image = browser.links.find_by_partial_text("Sample")
+    Valles_Marineris_hemisphere_image.click()
+    #Recreate soup
+    soup = BeautifulSoup(browser.html, 'html.parser')
+    Valles_Marineris_link = soup.find('img').get('src')
+    Valles_Marineris_full_link = "https://www.jpl.nasa.gov" + Valles_Marineris_link
+
 
 def scrape():
     news = scrape_news()
